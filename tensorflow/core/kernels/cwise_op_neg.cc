507d4754f638dd99844d50ca7d7c74a158f4d947
Kazuhisa Ishizaka <k-ishizaka@ay.jp.nec.com>
2018-09-21 16:31:31 +0900

    [VE] Add Neg

--- tensorflow/core/kernels/cwise_op_neg.cc
+++ tensorflow/core/kernels/cwise_op_neg.cc
@@ -15,6 +15,11 @@ limitations under the License.
 
 #include "tensorflow/core/kernels/cwise_ops_common.h"
 
+#ifdef TENSORFLOW_USE_VE
+#include "tensorflow/core/common_runtime/ve/ve_device.h"
+#include "tensorflow/core/common_runtime/dma_helper.h"
+#endif
+
 namespace tensorflow {
 REGISTER7(UnaryOp, CPU, "Neg", functor::neg, float, Eigen::half, double, int32,
           complex64, int64, complex128);
@@ -28,6 +33,69 @@ REGISTER_KERNEL_BUILDER(Name("Neg")
                             .TypeConstraint<int32>("T"),
                         UnaryOp<CPUDevice, functor::neg<int32>>);
 #endif  // TENSORFLOW_USE_SYCL
+
+#ifdef TENSORFLOW_USE_VE
+template <typename T>
+class VEUnaryOp : public OpKernel {
+  public:
+    explicit VEUnaryOp(OpKernelConstruction* ctx, std::string name) 
+      : OpKernel(ctx), name_(name) {}
+
+    void Compute(OpKernelContext* ctx) override {
+      const Tensor& inp = ctx->input(0);
+      Tensor* out = nullptr;
+      OP_REQUIRES_OK(ctx, ctx->forward_input_or_allocate_output(
+              {0}, 0, inp.shape(), &out));
+
+      struct _Tensor {
+        int dtype;
+        int data_format;
+        uint64_t addr;
+        int32_t dims;
+        int64_t nelems;
+        int64_t dim_size[8];
+      };
+
+      struct {
+        _Tensor in;
+        _Tensor out;
+      } args;
+
+      args.in.dtype = DataTypeToEnum<T>::v();
+      args.in.addr = (uint64_t)DMAHelper::base(&inp);
+      args.in.nelems = inp.NumElements();
+      args.out.addr = (uint64_t)DMAHelper::base(out);
+
+      VEDeviceContext* vectx = ctx->op_device_context<VEDeviceContext>();
+      Status s = vectx->Compute(name_.c_str(), (void*)&args, sizeof(args));
+      if (!s.ok())
+        ctx->SetStatus(s);
+    }
+
+  private:
+    std::string name_;
+};
+
+template<typename T>
+class VENegOp : public VEUnaryOp<T> {
+  public:
+    explicit VENegOp(OpKernelConstruction* ctx) 
+      : VEUnaryOp<T>(ctx, "Neg") {}
+};
+
+REGISTER_KERNEL_BUILDER(Name("Neg")
+                        .Device(DEVICE_VE)
+                        .TypeConstraint<float>("T"),
+                        VENegOp<float>);
+
+//REGISTER3(VEUnaryOp, VE, "Neg", functor::neg, float, double, int64);
+REGISTER_KERNEL_BUILDER(Name("Neg")
+                            .Device(DEVICE_VE)
+                            .HostMemory("x")
+                            .HostMemory("y")
+                            .TypeConstraint<int32>("T"),
+                        UnaryOp<CPUDevice, functor::neg<int32>>);
+#endif  // TENSORFLOW_USE_VE
 
 #if GOOGLE_CUDA
 REGISTER6(UnaryOp, GPU, "Neg", functor::neg, float, Eigen::half, double, int64,
