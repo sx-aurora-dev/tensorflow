db80b2815ab248c700e98aeca574f7213caec0b9
Yoshiyuki Ohno <ohno.yoshiyuki@nec.com>
2020-02-17 17:38:34 +0900

    [VE] update AddV2 ( add int64_t )

--- tensorflow/core/kernels/cwise_op_add_1.cc
+++ tensorflow/core/kernels/cwise_op_add_1.cc
@@ -67,12 +67,16 @@ REGISTER_KERNEL_BUILDER(Name("AddV2")
                         BinaryOp<CPUDevice, functor::add<int32>>);
 #endif  // TENSORFLOW_USE_SYCL
 
-#if TENSORFLOW_USE_VE
+#ifdef TENSORFLOW_USE_VE
 REGISTER_VE_BINARY_OP(Add, float, float, float);
 REGISTER_KERNEL_BUILDER(Name("AddV2")
                         .Device(DEVICE_VE)
                         .TypeConstraint<float>("T"),
                         VEAddOp<float, float>);
+REGISTER_KERNEL_BUILDER(Name("AddV2")
+                        .Device(DEVICE_VE)
+                        .TypeConstraint<int64>("T"),
+                        VEAddOp<int64, int64>);
 REGISTER_KERNEL_BUILDER(Name("Add")
                             .Device(DEVICE_VE)
                             .HostMemory("x")
